186d20759d752cf072c8921960c4dd596cf24a2b
Chew, Kar Joon <kar.joon.chew@merck.com>
2023-01-17 14:32:45 +0800

    complete add

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const ordonner = (a, b) => a.campaignTag > b.campaignTag;
+
 const app = Vue.createApp({
     data() {
         return {
@@ -6,16 +8,36 @@ const app = Vue.createApp({
                 { emailName: 'TW_Line_01', campaignTag: 'Line01' },
                 { emailName: 'TW_Line_02', campaignTag: 'Line01' }
             ],
-            message: 'Hello World from Joon222'
+            metadataAddInput: {
+                emailName: '',
+                campaignTag: ''
+            },
+            metadataEditInput: {
+                emailName: '',
+                campaignTag: ''
+            }
         };
     },
     methods: {
-        edit: function (index) {
+        edit(index) {
             this.editInput = this.campaigns[index];
             console.log(this.editInput);
             this.campaigns.splice(index, 1);
         },
-    },
+        add(index) {
+            this.campaigns.push({
+                emailName: this.metadataAddInput.emailName,
+                campaignTag: this.metadataAddInput.campaignTag
+            });
+
+            for (key in this.metadataAddInput) {
+                this.metadataAddInput[key] = '';
+            }
+            this.campaigns.sort(ordonner);
+            this.$refs.emailName.focus();
+        }
+    }
 });
 
 app.mount('#app');
+
